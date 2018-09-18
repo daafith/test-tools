d4185162de0a5f397c33e67292281976ab7345b9
David Baak <davidbaak@gmail.com>
2018-09-18 13:45:20 +0200

    added syntactic testing

---
+++ syntatic/README.md
@@ -0,0 +1,8 @@
+## Syntactical Testing
+Find (field) validation errors.
+
+### Special Characters
+List all standard ASCII special characters.
+```
+. specialCharacters.sh
+```
---
+++ syntatic/specialCharacters.sh
@@ -0,0 +1,12 @@
+printSpecialChar() {
+  for ((i=$1; i<=$2; i++))
+  do
+    printf "\\$(printf %o $i)"
+    printf "\n"
+  done
+}
+
+printSpecialChar 33 47
+printSpecialChar 58 64
+printSpecialChar 91 96
+printSpecialChar 123 126
