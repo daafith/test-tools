1c5d88b47b27d9f0e340a3693ad7379ec9bebfd7
David Baak <davidbaak@gmail.com>
2018-09-25 17:03:49 +0200

    phone numbers update

--- syntaticTesting/phoneNumbers.sh
+++ syntaticTesting/phoneNumbers.sh
@@ -1,7 +1,14 @@
 #!/bin/bash  
 CC=${1:-31}
+echo Disclaimer: You may need to play around with the length of the phone numbers
+echo ---
+echo - a phone number that should be accepted assuming the plus is allowed
 echo +"$CC"645672352
+echo - a phone number that should be accepted assuming the 00 is allowed
 echo 00"$CC"645672352
+echo - a phone number that should be accepted assuming the plus is allowed
 echo +"$CC"600"$CC"4567
+echo - a phone number that should be accepted assuming the 00 is allowed
 echo 00"$CC"600"$CC"4567
+echo - a phone number that should NOT be accepted assuming the plus is allowed
 echo +"$CC"6+"$CC"4567
